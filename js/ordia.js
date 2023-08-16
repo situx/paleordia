8b26acc4a7f932493c1241244fc83419bbe6f3af
Timo <situx@users.noreply.github.com>
2023-08-16 13:18:30 +0200

    update

--- js/ordia.js
+++ js/ordia.js
@@ -48,7 +48,7 @@ function extendColumnsFromCombinedCols(data,columns,sepchar){
 					splitted=data[i][col].split(sepchar)
 					console.log(splitted)
 					for(var j=0;j<splitted.length;j+=2){
-						if(!(isNumeric(splitted[j])) && !(splitted[j].includes("LAK"))){
+						if(!(isNumeric(splitted[j])) && !(splitted[i].length!=3 && splitted[j].includes("LAK"))){
 							aggcols[splitted[j]]=true
 						}
 					}
