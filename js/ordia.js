8ac151542c619e89684cd7bfa5931d36816a5cfc
Timo <situx@users.noreply.github.com>
2024-04-18 15:03:37 +0200

    fixes

--- js/ordia.js
+++ js/ordia.js
@@ -177,7 +177,7 @@ function convertDataTableData(data, columns, linkPrefixes={},linkParams={}) {
 			}else if(linkcount==1){
 				temp = '<a href="' +
 				(linkPrefixes[key] || "") + 
-				addParamsToLink(detectCorrectParameter(data[i][key].substr(36)),key,linkParams,data[i][key+'Label']+((key+'Label2' in data[i])?" "+data[i][key+'Label2']:""))+'"'
+				addParamsToLink(detectCorrectParameter(data[i][key].replace("http://www.wikidata.org/entity/","").replace("http://www.wikidata.org/prop/direct/","")),key,linkParams,data[i][key+'Label']+((key+'Label2' in data[i])?" "+data[i][key+'Label2']:"")) +'"'
 				if(typeof(h)!=="undefined" && data[i][key].includes(h)){
 					temp+=' style="color:red"'
 				}
