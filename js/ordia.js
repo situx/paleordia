0caf1ce93df1be15c20febb12cd19b7385daf04e
Timo <situx@users.noreply.github.com>
2024-08-19 23:58:04 +0200

    fixes

--- js/ordia.js
+++ js/ordia.js
@@ -373,11 +373,8 @@ function sparqlToDataTable(sparql, element, options={}) {
 
 	convertedData = convertDataTableData(simpleData.data, simpleData.columns, linkPrefixes=linkPrefixes,linkParams=linkParams);
 	console.log(convertedData)
-	console.log(convertedData.data.length)
-	console.log(options.divElem)
-	if(convertedData.data.length==0 && options.divElem!=""){
-		console.log(options.divElem)
-		$(options.divElem).hide();
+	if(convertedData.data.length==0 && divElem!=""){
+		$(divElem).hide();
 		return
 	}
 	var desc=false
