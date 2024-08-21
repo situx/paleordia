e7478b6a15b94b751936a1637cecc1af4153f67c
Timo <situx@users.noreply.github.com>
2024-08-21 16:34:11 +0200

    fixes

--- js/ordia.js
+++ js/ordia.js
@@ -202,6 +202,7 @@ function convertDataTableData(data, columns, linkPrefixes={},linkParams={}) {
 					var lastNumber = onlyNumbers.substring(onlyNumbers.length - 1);
 					var lastNumberIndex=firsturl.lastIndexOf(lastNumber)
 					sepchar=data[i][key].substring(lastNumberIndex+1,secondocc)
+					console.log("found new sepchar: "+sepchar)
 				}catch(err){
 					console.log("ERROR: "+err)
 				}
